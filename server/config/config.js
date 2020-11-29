83d9a7d46905b189e15161dd49c574cd687aba4b
Jorge Ariza <ageorge40@hotmail.com>
2020-11-28 19:37:40 -0600

    Adding rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,19 @@
 //======================
 
 process.env.PORT = process.env.PORT || 3000
+//======================
+//   Entorno
+//======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+//======================
+//   Database
+//======================
+let urlDB;
+ if (process.env.NODE_ENV === 'dev')
+ {
+     urlDB = 'mongodb://localhost:27017/Cafe';
+ }else {
+    urlDB = 'mongodb+srv://srvgeo:Jorg3arI@cluster0.uhnnf.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
+
